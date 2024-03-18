852824f2aca0e149e3da10525eeb3140807557b7
Fernando Zilion <142235593+zilionn@users.noreply.github.com>
2024-03-18 20:37:51 -0300

    Delete Lista Algoritmo - PYTHON/List01 directory

--- Lista Algoritmo - PYTHON/List01/Lista Algoritmo - PYTHON/List01.2/EX1LT01.2.py
+++
@@ -1,6 +0,0 @@
-class DiferencaMaior():
-    valor1 = int(input("Digite o primeiro valor inteiro: "))
-    valor2 = int(input("Digite o segundo valor inteiro: "))
-
-    diferenca = abs(max(valor1, valor2) - min(valor1, valor2))
-    print("A diferença entre os maiores valores é:", diferenca)
--- Lista Algoritmo - PYTHON/List01/Lista Algoritmo - PYTHON/List01.2/EX2LT01.2.py
+++
@@ -1,6 +0,0 @@
-class MaiorDois():
-    valor1 = float(input("Digite o primeiro valor real: "))
-    valor2 = float(input("Digite o segundo valor real: "))
-
-    maior = max(valor1, valor2)
-    print("O maior valor é:", maior)
